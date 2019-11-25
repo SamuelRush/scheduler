c1845c2272936f2b8d7c24a84f8dd126def78356
SamuelRush <samuelrush@gmail.com>
2019-11-25 16:57:05 -0700

    Day list included in storybook

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,7 +5,7 @@ const classNames = require("classnames");
 export default function DayListItem(props) {
   let dayClass = classNames({
     "day-list__item": true,
-    "day-list__item--selected": props.selected === true,
+    "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
 
