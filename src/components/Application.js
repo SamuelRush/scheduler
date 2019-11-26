386acf9f722ff2a914878b52a84d76b1bdc35d5f
SamuelRush <samuelrush@gmail.com>
2019-11-26 14:48:33 -0700

    Rendering the schedules on the website

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -21,8 +22,66 @@ const days = [
   }
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Sam Rush",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Leo Rush",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Max Rush",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const renderSchedule = appointments.map(appointment => (
+    <Appointment key={props.id} {...appointment} />
+  ));
   return (
     <main className="layout">
       <section className="sidebar">
@@ -42,7 +101,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {renderSchedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,21 +5,25 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  function reset() {
+  const reset = function() {
     setName("");
     setInterviewer(null);
+  };
+  function cancel() {
+    reset();
+    props.onCancel();
   }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            // name="name"
-            name={props.name}
+            // name={name}
             type="text"
+            value={name}
             placeholder="Enter Student Name"
-            onChange={name => setName(name)}
+            onChange={event => setName(event.target.value)}
             /*
           This must be a controlled component
         */
@@ -27,16 +31,17 @@ export default function Form(props) {
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          interviewer={props.interviewer}
-          onChange={interviewer => setInterviewer(interviewer)}
+          interviewer={interviewer}
+          // onChange={interviewer => setInterviewer(interviewer)}
+          setInterviewer={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={(props.onCancel, reset)}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={props.onSave}>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>
             Save
           </Button>
         </section>
