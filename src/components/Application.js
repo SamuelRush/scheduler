0d2e0e52fcf10f7b0659c78d607f3dcb627efa7d
SamuelRush <samuelrush@gmail.com>
2019-11-27 10:08:43 -0700

    Selector code for interviewers and tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAppointmentsForDay } from "helpers/selectors";
+import { getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -13,26 +14,37 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const appointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState({ ...state, days });
 
-  const renderSchedule = appointments.map(appointment => (
-    <Appointment key={props.id} {...appointment} />
-  ));
+  const renderSchedule = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={appointment.interview}
+      />
+    );
+  });
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get(`http://localhost:8001/api/days`)),
-      Promise.resolve(axios.get(`http://localhost:8001/api/appointments`))
+      Promise.resolve(axios.get(`http://localhost:8001/api/appointments`)),
+      Promise.resolve(axios.get(`http://localhost:8001/api/interviewers`))
     ]).then(all => {
-      // console.log(all[0].data, all[1].data);
-      const [days, appointments] = all;
+      // console.log(all[0].data, all[1].data, all[2].data);
+      const [days, appointments, interviewers] = all;
       setState(prev => ({
         day: "Monday",
         days: all[0].data,
-        appointments: all[1].data
+        appointments: all[1].data,
+        interviewers: all[2].data
       }));
     });
     // axios
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,3 +16,25 @@ export function getAppointmentsForDay(state, day) {
 
   return apptmt;
 }
+
+export function getInterview(state, appointment) {
+  let interviewData = {};
+  if (appointment !== null) {
+    const interNum = appointment["interviewer"];
+    for (const iId in state.interviewers) {
+      if (Number(iId) === Number(interNum)) {
+        interviewData.student = appointment["student"];
+        interviewData.interviewer = {};
+        interviewData.interviewer.id = state.interviewers[iId].id;
+        interviewData.interviewer.name = state.interviewers[iId].name;
+        interviewData.interviewer.avatar = state.interviewers[iId].avatar;
+      }
+    }
+  }
+  if (Object.keys(interviewData).length === 0) {
+    return null;
+  } else {
+    return interviewData;
+  }
+}
+//WHEN THIS IS WORKING WE NEED TO TRANSFER IT TO APPLICATIONS.
