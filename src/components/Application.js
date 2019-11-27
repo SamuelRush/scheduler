3b751fe9671053201e21164b804d1a2fede61a3a
SamuelRush <samuelrush@gmail.com>
2019-11-26 18:55:15 -0700

    API toI to get users info

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0
-  }
-];
+const axios = require("axios").default;
+//need to run API server and this
 
 const appointments = [
   {
@@ -79,9 +63,14 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const renderSchedule = appointments.map(appointment => (
     <Appointment key={props.id} {...appointment} />
   ));
+  axios
+    .get(`http://localhost:8001/api/days`)
+    .then(response => setDays(response.data));
+
   return (
     <main className="layout">
       <section className="sidebar">
