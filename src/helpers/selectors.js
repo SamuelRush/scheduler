0eb0ebc9eb1393a20c80147d695859fb111b3d9d
SamuelRush <samuelrush@gmail.com>
2019-11-27 18:53:02 -0700

    Selector for get users by day, have interview name showing up on day summary

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,6 +17,35 @@ export function getAppointmentsForDay(state, day) {
   return apptmt;
 }
 
+export function getInterviewersForDay(state, day) {
+  // function getInterviewersForDay(state, day) {
+  const ivw = [];
+  for (let dayId of state.days) {
+    if (dayId.name === day) {
+      const appointments = dayId.appointments;
+      for (let apptmtID of appointments) {
+        for (const aId in state.appointments) {
+          if (Number(aId) === Number(apptmtID)) {
+            if (state.appointments[aId].interview !== null) {
+              for (const iId in state.interviewers) {
+                if (
+                  Number(iId) ===
+                  Number(state.appointments[aId].interview.interviewer)
+                ) {
+                  ivw.push(state.interviewers[iId]);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return ivw;
+}
+// console.log(getInterviewersForDay(state, "Monday"));
+
 export function getInterview(state, appointment) {
   let interviewData = {};
   if (appointment !== null) {
